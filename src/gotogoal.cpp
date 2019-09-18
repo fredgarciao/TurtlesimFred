b0c5e5f50914b36a073aa3a1f0c28b5a9172bcf9
fredlaranja <frederico.garcia.oliveira@gmail.com>
2019-09-18 19:16:22 -0300

    adding comments

--- src/gotogoal.cpp
+++ src/gotogoal.cpp
@@ -13,12 +13,12 @@ double y_actual;
 double theta_actual;
 	
 // Error variable -> Pose that you want - Actual Pose
-double error = 0.001;
+double error = 0.1;
 // Callback function to get the turtle pose
 void callBack(const turtlesim::Pose& msg2){
 	ROS_INFO("Pose x = %f", msg2.x);
 	ROS_INFO("Pose y = %f", msg2.y);
-	//ROS_INFO("theta = %f", msg2.theta);
+	ROS_INFO("theta = %f", msg2.theta);
 	
     // Assigning pose variables
 	x_actual = msg2.x;
@@ -42,52 +42,40 @@ int main(int argc, char**argv){
     // Subscriber of turtle pose
 	ros::Subscriber sub = n.subscribe("turtle1/pose", 1000, callBack);
 	
-	ros::Rate loop_rate(50);
-	
-	int count = 0;
-	
-	
+	ros::Rate loop_rate(50); // loop
+	// Counts
+	int count = 0; 
+	// pose msg (object)
 	turtlesim::Pose msg2;
 	
 	while(ros::ok()){
-		geometry_msgs::Twist msg1;
+		// Vel msg (object)
+		geometry_msgs::Twist msg1; 
 		
-		//Comando para X e Y e Theta
+		//X, Y and theta command
+		// error in x
 		double x_error = x_pose - x_actual;
+		// error in y
 		double y_error = y_pose - y_actual;
+		// theta error
 		double theta_error = atan2 ( ( y_error ) , ( x_error ) ) - theta_actual;
+		// Linear_error
 		double linear_error = sqrt(pow(x_error, 2.0) + pow(y_error, 2.0));
-								   
+		// Euclidian distance		   
 		if( linear_error >= error ){
-			//msg1.linear.x = ((-x_actual + x_pose)/std::abs((x_actual - x_pose)) )* 0.5;
-			//msg1.linear.y = ((-y_actual + y_pose)/std::abs((y_actual - y_pose)) )* 0.5;
-			
-			double Vx= linear_error * cos(theta_error) * 0.7;
+			double Vx= linear_error * cos(theta_error) * 0.2;
+			// Linear velocity
 			double w = theta_error * 0.5;
-			
-			
+			// theta velocity
 			msg1.linear.x = Vx;
+
 			msg1.angular.z = w;
-			
-			
-			ROS_INFO("error ang = %f", theta_error);
-			
-			ROS_INFO("Vx = %f", msg1.linear.x);
-			//ROS_INFO("Vy = %f", msg1.linear.y);
-			ROS_INFO("x_error = %f", x_error);
-			ROS_INFO("y_error = %f", y_error);
-			ROS_INFO("linear_error = %f", linear_error );
-			//double k = ((-x_actual + x_pose)/std::abs((x_actual - x_pose)) );
-			//ROS_INFO("%f", k );
 		}
-		
 		else{
 			msg1.linear.x =0.;
 			msg1.angular.z = 0.;
-			ROS_INFO("linear_error = %f", linear_error );
-			//ROS_INFO("else is ok");
 		}
-		
+	
 		pub.publish(msg1);
 		
 		ros::spinOnce();
